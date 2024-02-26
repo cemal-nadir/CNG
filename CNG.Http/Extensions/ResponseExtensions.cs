6201804972247fe16f1d7a70567f9c66fd4c7275
Cemal Nadir <cemal_nadir1999@hotmail.com>
2024-02-27 00:45:07 +0300

    bug fixed

--- CNG.Http/Extensions/ResponseExtensions.cs
+++ CNG.Http/Extensions/ResponseExtensions.cs
@@ -5,47 +5,57 @@ using CNG.Http.Helpers;
 
 namespace CNG.Http.Extensions
 {
-	public static class ResponseExtensions
-	{
-		public static async Task<HttpClientResponse<TResponse>> PrepareResponse<TResponse>(
-			this HttpResponseMessage response, ResponseType responseType = ResponseType.Json,
-			ExceptionResponseType? exceptionResponseType = null,
-			Func<string, ExceptionResponseType?, string>? exceptionHandler = null,
-			CancellationToken cancellationToken = default)
-		{
-			if (response.StatusCode == HttpStatusCode.Unauthorized)
-				return new HttpClientErrorResponse<TResponse>("Unauthorized", response.StatusCode);
+    public static class ResponseExtensions
+    {
+        public static async Task<HttpClientResponse<TResponse>> PrepareResponse<TResponse>(
+            this HttpResponseMessage response, ResponseType responseType = ResponseType.Json,
+            ExceptionResponseType? exceptionResponseType = null,
+            Func<string, ExceptionResponseType?, string>? exceptionHandler = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+                return new HttpClientErrorResponse<TResponse>("Unauthorized", response.StatusCode);
 
-			var message = await response.Content.ReadAsStringAsync(cancellationToken);
+            var message = await response.Content.ReadAsStringAsync(cancellationToken);
 
-			if (response.IsSuccessStatusCode)
-				return new HttpClientSuccessResponse<TResponse>(
-					responseType is ResponseType.Json
-						? JsonHelper.DeserializeObject<TResponse>(message)
-						: XmlHelper.DeserializeObject<TResponse>(message), response.Headers);
+            if (!response.IsSuccessStatusCode)
+                return new HttpClientErrorResponse<TResponse>(
+                    exceptionHandler != null ? exceptionHandler(message, exceptionResponseType) : message,
+                    response.Headers,
+                    response.StatusCode);
+            try
+            {
+                return new HttpClientSuccessResponse<TResponse>(
+                    responseType is ResponseType.Json
+                        ? JsonHelper.DeserializeObject<TResponse>(message)
+                        : XmlHelper.DeserializeObject<TResponse>(message), response.Headers);
+            }
+            catch (Exception ex)
+            {
+                return new HttpClientErrorResponse<TResponse>(
+                    exceptionHandler != null ? exceptionHandler($"{message} | {ex.Message}", exceptionResponseType) : $"{message} | {ex.Message}", response.Headers,
+                    response.StatusCode);
+            }
 
-			return new HttpClientErrorResponse<TResponse>(
-				exceptionHandler != null ? exceptionHandler(message, exceptionResponseType) : message, response.Headers,
-				response.StatusCode);
-		}
+        }
 
-		public static async Task<HttpClientResponse> PrepareResponse(this HttpResponseMessage response,
-			ExceptionResponseType? exceptionResponseType = null,
-			Func<string, ExceptionResponseType?, string>? exceptionHandler = null,
-			CancellationToken cancellationToken = default)
-		{
-			if (response.StatusCode == HttpStatusCode.Unauthorized)
-				return new HttpClientErrorResponse("Unauthorized", response.StatusCode);
+        public static async Task<HttpClientResponse> PrepareResponse(this HttpResponseMessage response,
+            ExceptionResponseType? exceptionResponseType = null,
+            Func<string, ExceptionResponseType?, string>? exceptionHandler = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+                return new HttpClientErrorResponse("Unauthorized", response.StatusCode);
 
-			var message = await response.Content.ReadAsStringAsync(cancellationToken);
+            var message = await response.Content.ReadAsStringAsync(cancellationToken);
 
-			if (response.IsSuccessStatusCode)
-				return new HttpClientSuccessResponse(
-					message, response.Headers);
+            if (response.IsSuccessStatusCode)
+                return new HttpClientSuccessResponse(
+                    message, response.Headers);
 
-			return new HttpClientErrorResponse(
-				exceptionHandler != null ? exceptionHandler(message, exceptionResponseType) : message, response.Headers,
-				response.StatusCode);
-		}
-	}
+            return new HttpClientErrorResponse(
+                exceptionHandler != null ? exceptionHandler(message, exceptionResponseType) : message, response.Headers,
+                response.StatusCode);
+        }
+    }
 }
