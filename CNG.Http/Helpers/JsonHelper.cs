359e8af5c3e5169f4724a07442749ccc496902c9
cemal <cemalnadirbusiness@gmail.com>
2024-02-27 00:42:48 +0300

    updated

--- CNG.Http/Helpers/JsonHelper.cs
+++ CNG.Http/Helpers/JsonHelper.cs
@@ -6,9 +6,7 @@ namespace CNG.Http.Helpers
 	public static class JsonHelper
 	{
 		public static string SerializeObject<T>(T data) =>
-			JsonConvert.SerializeObject(data,
-				settings: new JsonSerializerSettings()
-					{ ContractResolver = new CamelCasePropertyNamesContractResolver() });
+			JsonConvert.SerializeObject(data);
 
 		public static T? DeserializeObject<T>(string jsonString) => JsonConvert.DeserializeObject<T>(jsonString);
 	}
